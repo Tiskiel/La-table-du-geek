e065ba388cacd2609848bd8f34e910311ca046b0
zekotage <marino.valentin@gmail.com>
2022-10-18 16:39:47 +0200

    modif recette et logo home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import logo from '../../Assets/images/logo-ALTG.png'
+import logo from '../../Assets/images/logo-ALTG2.png'
 
 export default function Home() { 
 
@@ -11,7 +11,7 @@ export default function Home() {
                 <img src={logo} alt='' />
             </div>
             <div className='text-center font-Honey'>
-                <h1 className='text-6xl text-rose-800'>[Bienvenue à La table du geek]</h1>
+                <h1 className='text-6xl text-rose-800'>[Description]</h1>
             </div>           
         </div>
     )
--- src/Pages/Recepes/Recepes.jsx
+++ src/Pages/Recepes/Recepes.jsx
@@ -12,11 +12,42 @@ export default function Recepes() {
     console.log(recipe);
 
     return (
-        <div className='bg-gray-400 h-screen'>
-            <img className='rounded-full h-40' src={recipe.imgDirect} alt='' />
-            <p>
-                Bienvenu sur la page de {recipe.name}
-            </p>
+        <div className='h-screen bg-gray-400'>
+            <div className="flex-col m-2 mt-20 p-3 bg-[url('Assets/images/txt.png')] bg-cover">
+                <div className='flex-col'>
+                    <img className='rounded-full w-60 shadow-2xl mt-2' src={recipe.imgDirect} alt='' />
+                </div>
+                <div className="">
+                    <div className='flex-col p-5 text-xl'>
+                        <p className='text-center'>
+                            <u>{recipe.name}</u>
+                        </p>
+                    </div>
+                    <div className='flex-col mt-1'>
+                        <p className='text-justify ml-32 mt-3'>
+                            <b>Recette</b>
+                            <br />
+                            cuire les nouilles
+                            <br />
+                            cuire les boulettes
+                            <br />
+                            mélanger le tout 
+                            <br />
+                            cuire les nouilles
+                            <br />
+                            cuire les boulettes
+                            <br />
+                            mélanger le tout 
+                            <br />
+                            cuire les nouilles
+                            <br />
+                            cuire les boulettes
+                            <br />
+                            mélanger le tout 
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
